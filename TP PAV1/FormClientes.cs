085ab48ed00afb90e28d7edb17f62ba22b46cc83
garipfederico <garipfederico@gmail.com>
2021-05-21 00:05:00 -0300

    ABM 1ra entrega funcional

--- TP PAV1/FormClientes.cs
+++ TP PAV1/FormClientes.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TP_PAV1.AD;
+using TP_PAV1.Entidades;
 
 namespace TP_PAV1
 {
@@ -19,7 +21,276 @@ namespace TP_PAV1
 
         private void FormClientes_Load(object sender, EventArgs e)
         {
+            LimpiarCampos();
+            btnActualizar.Enabled = false;
+            btnEliminar.Enabled = false;
+            CargarComboTiposDocumentos();
+            CargarComboBarrios();
+            CargarGrilla();
+            cmbTipoDoc.Enabled = true;
+            txtNroDoc.Enabled = true;
+        }
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtFechaNacimiento.Text = "";
+
+            txtNroDoc.Text = "";
+
+            rdBtnMasculino.Checked = false;
+            rdBtnFemenino.Checked = false;
+            rdBtnFemenino.Checked = false;
+
+            txtCalle.Text = "";
+            txtNroDomicilio.Text = "";
+
+            rdBtnSoltero.Checked = false;
+            rdBtnCasado.Checked = false;
+
+          
+        }
+
+        private void btnLimpiarDatos_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            btnEliminar.Enabled = false;
+            btnGuardarCliente.Enabled = true;
 
         }
+
+        private void CargarComboTiposDocumentos()
+        {
+
+            try
+            {
+                cmbTipoDoc.DataSource = AD_Cliente.ObtenerTabla("tipo_documento");
+                cmbTipoDoc.DisplayMember = "nombre_tipo_documento";
+                cmbTipoDoc.ValueMember = "id_tipo_documento";
+                cmbTipoDoc.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar combo tipo documento");
+            }
+        }
+
+        private void CargarComboBarrios()
+        {
+            try
+            {
+                cmbBarrio.DataSource = AD_Cliente.ObtenerTabla("barrios");
+                cmbBarrio.DisplayMember = "nombre_barrio";
+                cmbBarrio.ValueMember = "id_barrio";
+                cmbBarrio.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar combo barrios");
+            }
+        }
+
+        private void CargarGrilla()
+        {
+            try
+            {
+                grdClientes.DataSource = AD_Cliente.ObtenerListadoClientesReducido();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error al obtener clientes");
+            }
+
+        }
+
+        private Cliente ObtenerDatosCliente()
+        {
+            Cliente c = new Cliente();
+            c.NombreCliente = txtNombre.Text.Trim();
+            c.ApellidoCliente = txtApellido.Text.Trim();
+            c.FechaNacimientoCliente = DateTime.Parse(txtFechaNacimiento.Text);
+
+            c.TipoDocumentoCliente = (int)(cmbTipoDoc.SelectedValue);
+            c.DocumentoCliente = txtNroDoc.Text.Trim();
+
+            if (rdBtnFemenino.Checked)
+            {
+                c.SexoCliente = 1;
+            }
+            else if (rdBtnMasculino.Checked)
+            {
+                c.SexoCliente = 2;
+            }
+            else if (rdBtnOtro.Checked)
+            {
+                c.SexoCliente = 3;
+            }
+
+            c.BarrioCliente = (int)(cmbBarrio.SelectedValue);
+            c.CalleCliente = txtCalle.Text.Trim();
+            c.NroCasaCliente = int.Parse(txtNroDomicilio.Text.Trim());
+
+            if (rdBtnSoltero.Checked)
+            {
+                c.EstadoCivilCliente = 1;
+            }
+            else if (rdBtnCasado.Checked)
+            {
+                c.EstadoCivilCliente = 2;
+            }
+            
+            return c;
+        }
+
+        private void btnGuardarCliente_Click(object sender, EventArgs e)
+        {
+            Cliente c = ObtenerDatosCliente();
+            bool resultado = AD_VariosXFede.AgregaClienteABD(c);
+            if (resultado)
+            {
+                MessageBox.Show("Cliente agregado correctamente");
+                LimpiarCampos();
+                CargarComboBarrios();
+                CargarComboTiposDocumentos();
+                CargarGrilla();
+            }
+            else
+            {
+                MessageBox.Show("No se pudo cargar el cliente");
+            }
+        }
+
+        private void grdClientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int indice = e.RowIndex;
+            if (indice >= 0)
+            {
+                btnActualizar.Enabled = true;
+                btnEliminar.Enabled = true;
+                btnGuardarCliente.Enabled = false;
+                cmbTipoDoc.Enabled = false;
+                txtNroDoc.Enabled = false;
+
+
+                DataGridViewRow filaSeleccionada = grdClientes.Rows[indice];
+                string nroDoc = filaSeleccionada.Cells["nroDoc"].Value.ToString();
+                string tipoDoc = filaSeleccionada.Cells["tipoDoc"].Value.ToString();
+
+                Cliente c = AD_VariosXFede.ObtenerIdClienteXDocumento(nroDoc);
+
+                LimpiarCampos();
+                CargarCampos(c);
+            }
+        }
+
+        private void CargarCampos(Cliente c)
+        {
+            txtNombre.Text = c.NombreCliente;
+            txtApellido.Text = c.ApellidoCliente;
+            DateTime fecha = c.FechaNacimientoCliente;
+            string dia = "";
+            string mes = "";
+            string año = "";
+            dia = fecha.Date.Day.ToString();
+            if (dia.Length == 1)
+            {
+                dia = "0" + dia;
+            }
+            mes = fecha.Date.Month.ToString();
+            if (mes.Length == 1)
+            {
+                mes = "0" + mes;
+            }
+            año = fecha.Date.Year.ToString();
+            txtFechaNacimiento.Text = dia + mes + año;
+
+            cmbTipoDoc.SelectedValue = c.TipoDocumentoCliente;
+            txtNroDoc.Text = c.DocumentoCliente;
+
+            if (c.SexoCliente == 1)
+            {
+                rdBtnFemenino.Checked = true;
+            }
+            else if (c.SexoCliente == 2)
+            {
+                rdBtnMasculino.Checked = true;
+            }
+            else
+            {
+                rdBtnOtro.Checked = true;
+            }
+
+            cmbBarrio.SelectedValue = c.BarrioCliente;
+            txtCalle.Text = c.CalleCliente;
+            txtNroDomicilio.Text = c.NroCasaCliente.ToString();
+            
+
+            if (c.EstadoCivilCliente == 1)
+            {
+                rdBtnSoltero .Checked = true;
+            }
+            else if(c.EstadoCivilCliente == 2)
+            {
+                rdBtnCasado.Checked = true;
+            }
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            string doc = grdClientes.CurrentRow.Cells["nroDoc"].Value.ToString();
+            string tipoDoc = grdClientes.CurrentRow.Cells["tipoDoc"].Value.ToString();
+            Cliente cli = ObtenerDatosCliente();
+            bool resultado = AD_Cliente.ActualizarCliente(cli);
+
+            if (resultado)
+            {
+                MessageBox.Show("Cliente actualizado con exito");
+                LimpiarCampos();
+                CargarGrilla();
+               
+                CargarComboTiposDocumentos();
+                btnActualizar.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Error al actualizar Cliente");
+            }
+        }
+
+        private void grdClientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            
+            Cliente c = ObtenerDatosCliente();
+            string msjAlert = "Desea eliminar el cliente: \n " + c.ApellidoCliente + " " + c.NombreCliente + " Tipo y nro de doc.: " + c.TipoDocumentoCliente + " " + c.DocumentoCliente;
+            
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            MessageBox.Show(msjAlert ,"Error", buttons);
+            
+
+            bool resultado = AD_Cliente.EliminarCliente(c);
+
+            if (resultado)
+            {
+                MessageBox.Show("Cliente Eliminado exitosamente");
+                LimpiarCampos();
+                CargarGrilla();
+
+                CargarComboTiposDocumentos();
+                btnActualizar.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Error al actualizar Cliente");
+            }
+        }
     }
+
 }
--- TP PAV1/FormProfesiones.Designer.cs
+++ TP PAV1/FormProfesiones.Designer.cs
@@ -28,7 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProfesion = new System.Windows.Forms.TextBox();
+            this.grdProfesiones = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.id_Profesiones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre_profesion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.grdProfesiones)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(17, 281);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 44);
+            this.btnActualizar.TabIndex = 36;
+            this.btnActualizar.Text = "Actualizar Profesion";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(17, 216);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 44);
+            this.btnGuardar.TabIndex = 33;
+            this.btnGuardar.Text = "Guardar Profesion";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(17, 349);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 44);
+            this.btnEliminar.TabIndex = 34;
+            this.btnEliminar.Text = "Eliminar Profesion";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Profesion:";
+            // 
+            // txtProfesion
+            // 
+            this.txtProfesion.Location = new System.Drawing.Point(98, 119);
+            this.txtProfesion.Name = "txtProfesion";
+            this.txtProfesion.Size = new System.Drawing.Size(147, 20);
+            this.txtProfesion.TabIndex = 30;
+            // 
+            // grdProfesiones
+            // 
+            this.grdProfesiones.AllowUserToAddRows = false;
+            this.grdProfesiones.AllowUserToDeleteRows = false;
+            this.grdProfesiones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdProfesiones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_Profesiones,
+            this.nombre_profesion});
+            this.grdProfesiones.Location = new System.Drawing.Point(266, 73);
+            this.grdProfesiones.Name = "grdProfesiones";
+            this.grdProfesiones.ReadOnly = true;
+            this.grdProfesiones.Size = new System.Drawing.Size(277, 493);
+            this.grdProfesiones.TabIndex = 32;
+            this.grdProfesiones.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdProfesiones_CellClick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(139)))), ((int)(((byte)(204)))));
+            this.label2.Location = new System.Drawing.Point(177, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 24);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Administrar Profesiones";
+            // 
+            // id_Profesiones
+            // 
+            this.id_Profesiones.DataPropertyName = "id_profesion";
+            this.id_Profesiones.HeaderText = "id";
+            this.id_Profesiones.Name = "id_Profesiones";
+            this.id_Profesiones.ReadOnly = true;
+            this.id_Profesiones.Width = 35;
+            // 
+            // nombre_profesion
+            // 
+            this.nombre_profesion.DataPropertyName = "nombre_profesion";
+            this.nombre_profesion.HeaderText = "Profesión";
+            this.nombre_profesion.Name = "nombre_profesion";
+            this.nombre_profesion.ReadOnly = true;
+            this.nombre_profesion.Width = 200;
             // 
             // FormProfesiones
             // 
@@ -36,14 +137,34 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
             this.ClientSize = new System.Drawing.Size(555, 684);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProfesion);
+            this.Controls.Add(this.grdProfesiones);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MinimumSize = new System.Drawing.Size(555, 684);
             this.Name = "FormProfesiones";
             this.Text = "FormProfesiones";
+            this.Load += new System.EventHandler(this.FormProfesiones_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdProfesiones)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProfesion;
+        private System.Windows.Forms.DataGridView grdProfesiones;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_Profesiones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre_profesion;
     }
 }
