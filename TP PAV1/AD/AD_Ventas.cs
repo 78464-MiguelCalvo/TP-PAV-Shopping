24b347f1a0a7d808564afa512511333986da59af
75758 Gerardo Ormeño <geryormeno@gmail.com>
2021-06-14 22:07:11 -0300

    ReportesGeraTerminados

--- TP PAV1/AD/AD_Ventas.cs
+++ TP PAV1/AD/AD_Ventas.cs
@@ -174,5 +174,42 @@ namespace TP_PAV1.AD
             }
         }
 
+
+        public static DataTable obtenerVentasPorCliente()
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["cadenaTP1"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+
+                string consulta = "VentasPorCliente";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+
     }
 }
